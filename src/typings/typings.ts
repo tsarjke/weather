e8aa79f41d38ebfe0265221f09857295cc221adf
Ivan Tsarev <ivantsaryok@yandex.ru>
2023-09-11 21:38:02 +0400

    add forecast type template and correct location (lat and lon instead of if)

--- src/typings/typings.ts
+++ src/typings/typings.ts
@@ -1,10 +1,40 @@
 export type Location = {
   name: string;
   region: string;
+  country: string;
+  lat: number;
+  lon: number;
   id: number;
 };
 
 export type InputOption = {
   text: string;
   value: string | number;
+}
+
+export type CurrentWeather = {
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  temp_c: number;
+  temp_f: number;
+}
+
+export type ForecastForDay = {
+  date: string;
+  date_epoch: number;
+  day: {
+    avgtemp_c: number;
+    avgtemp_f: number;
+    condition: {
+      text: string;
+      code: number;
+    }
+  }
+}
+
+export type Forecast = {
+  current: CurrentWeather;
+  forecast: [ForecastForDay];
+  location: Location;
 }
