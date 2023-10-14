313b47b8cb0feeb6a1ef7ec3eb095c3638572a21
Ivan Tsarev <ivantsaryok@yandex.ru>
2023-10-14 20:59:44 +0400

    add footer with contacts

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="footer">
+    <ul class="footer__list">
+      <li v-for="item in links" :key="item.name" class="footer__item">
+        <a :href="item.link" target="_blank" class="footer__link">
+          <img :src="item.icon" :alt="item.name">
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import linkedinIcon from '@/assets/img/icons/footer/linkedin.svg';
+import githubIcon from '@/assets/img/icons/footer/github.svg';
+import emailIcon from '@/assets/img/icons/footer/email.svg';
+import telegramIcon from '@/assets/img/icons/footer/telegram.svg';
+
+export default defineComponent({
+  name: 'PageFooter',
+  setup() {
+    const links = ref([
+      { name: 'linkedin', icon: linkedinIcon, link: 'https://www.linkedin.com/in/ivan~tsarev/' },
+      { name: 'github', icon: githubIcon, link: 'https://github.com/tsarjke' },
+      { name: 'email', icon: emailIcon, link: 'mailto:ivantsarb@gmail.com' },
+      { name: 'telegram', icon: telegramIcon, link: 'tg://resolve?domain=tsarjke' },
+    ]);
+
+    return { links };
+  },
+});
+</script>
+
+<style lang="scss">
+.footer {
+
+  &__list {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+  }
+
+  &__link {
+    display: block;
+    width: 3.5rem;
+    height: 3.5rem;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+</style>
